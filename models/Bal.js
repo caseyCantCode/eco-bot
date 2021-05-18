a30026000200a462066732df9a27c09bdd78775c
caseyCantCode <77986712+caseyCantCode@users.noreply.github.com>
2021-05-18 16:50:16 -0600

    Delete Bal.js

--- models/Bal.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose')
-module.exports = model(`Eco`, Schema({
-    GuildID: String,
-    UserID: String,
-    WorkCooldown: String,
-    DailyCooldown: String,
-    Coins: Number,
-    Bank: { type: Number, default: 0 }
-}))
